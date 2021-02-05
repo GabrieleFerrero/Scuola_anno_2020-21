0f714f06a47854768025402aed528c2ca5e72959
Gabriele Ferrero <63954224+GabrieleFerrero@users.noreply.github.com>
2021-02-05 13:35:19 +0100

    Add files via upload

---
+++ Informatica/java/esercitazioni/Concatenatore_Nome_Cognome/src/App.java
@@ -0,0 +1,12 @@
+/*
+Scrivere una classe Java Concatena2 che stampi: le prime tre lettere 
+del proprio nome concatenate con le prime
+tre lettere del proprio cognome tutto in maiuscolo.
+Utilizzare il metodo concat ed il metodo substring
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
